c26b67b2695db8b33402ddd52002087d940a28e0
Steven Sullivan <53283472+binolt@users.noreply.github.com>
2021-09-11 15:37:48 -0400

    update next.config.js and add test api route

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    reactStrictMode: true,
+    env: {
+      "MONGODB_URI": "mongodb+srv://admin:eq6USAZD17e3@cluster0.pakjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    }
+  }
