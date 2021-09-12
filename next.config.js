740bd2a67266964ac13efcfcd5e5b70622bb8164
Steven Sullivan <53283472+binolt@users.noreply.github.com>
2021-09-12 17:19:53 -0400

    update env variables

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-// module.exports = {
-//     reactStrictMode: true,
-//     env: {
-//       "MONGODB_URI": "mongodb+srv://admin:eq6USAZD17e3@cluster0.pakjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-//       "TOKEN_SECRET": "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYzMTQ2NzQ4OCwiaWF0IjoxNjMxNDY3NDg4fQ.dK6oxCBcbSZRUKctB8Kjumm7-W2YrFkE0QF0WVtvTmM",
-//       "GITHUB_CLIENT_ID": "da1368c1a4fb4e9be4ae",
-//       "GITHUB_CLIENT_SECRET": "1ba06271db463100dea70645f0b06c6652338600",
-//       "GITHUB_CALLBACK_URL": "http://localhost:3000/api/auth/github/callback"
-//     }
-// }
+module.exports = {
+    reactStrictMode: true,
+    env: {
+      "MONGODB_URI": "mongodb+srv://admin:eq6USAZD17e3@cluster0.pakjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      "TOKEN_SECRET": "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYzMTQ2NzQ4OCwiaWF0IjoxNjMxNDY3NDg4fQ.dK6oxCBcbSZRUKctB8Kjumm7-W2YrFkE0QF0WVtvTmM",
+      "GITHUB_CLIENT_ID": "9e2c12e4c82dddd698a3",
+      "GITHUB_CLIENT_SECRET": "b51791989ae67c85744dd1325c11d614c0c0c2d8",
+      "GITHUB_CALLBACK_URL": "https://test-build.d8ottqgcqlwb0.amplifyapp.com/api/auth/github/callback"
+    }
+}
